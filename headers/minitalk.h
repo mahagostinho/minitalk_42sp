92064b02301578b58cf2638c26969171dd4effaa
mahagostinho <marcella.c.agostinho@gmail.com>
2023-03-16 17:49:30 -0300

    criar header comum aos arquivos .c

--- headers/minitalk.h
+++ headers/minitalk.h
@@ -6,21 +6,14 @@
 /*   By: marcarva <marcarva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 16:13:09 by marcarva          #+#    #+#             */
-/*   Updated: 2023/03/16 17:11:15 by marcarva         ###   ########.fr       */
+/*   Updated: 2023/03/16 17:48:05 by marcarva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
 # include <signal.h>
-# include <sys/types.h>
 # include "../libs/libft/headers/libft.h"
-# include <stdbool.h>
-
-void	print_char(int sig, siginfo_t *siginfo, void *context);
 
 #endif
