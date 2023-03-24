c9c6af04f911a27c701d5053db4cad3cea65c95f
mahagostinho <marcella.c.agostinho@gmail.com>
2023-03-24 15:13:43 -0300

    create handler signal function

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: marcarva <marcarva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 16:13:03 by marcarva          #+#    #+#             */
-/*   Updated: 2023/03/24 11:57:05 by marcarva         ###   ########.fr       */
+/*   Updated: 2023/03/24 15:09:58 by marcarva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,15 @@ void	error_message(char *message)
 
 void	print_message(int signal, siginfo_t *info, void *ucontext)
 {
-
+	(void)ucontext;
+	if (signal == SIGUSR1) //there is message sended	
+	{	
+		ft_printf("Received\n");
+		//send signal to client asking for more
+		kill(info->si_pid, SIGUSR1);
+	}
+	//else if SIGUSR2 was sended by the client means there is no message
+	kill(info->si_pid, SIGUSR2);
 }
 
 int	main(void)
@@ -38,6 +46,9 @@ int	main(void)
 	server_sa.sa_sigaction = print_message;
 	process_id = getpid();
 	ft_printf("PID: %d\n", process_id);
+	/* Sigaction catch the signals sended by client (message)
+	SIGUSR1 means "Client sending things to you and 
+	SIGUSR2 means the message ended*/
 	if (sigaction(SIGUSR1, &server_sa, NULL) == -1)
 		error_message("Failed to set SIGUSR1");
 	if (sigaction(SIGUSR2, &server_sa, NULL) == -1)
